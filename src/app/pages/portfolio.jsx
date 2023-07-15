f98f8f69467b147c0f0bfae8a5cd4541a6e273d2
Ben Euhus <ben.euhus@emsibg.com>
2023-07-14 22:07:27 -0700

    init

---
+++ src/app/pages/portfolio.jsx
@@ -0,0 +1,4 @@
+export default function Portfolio() {
+    return <h1>Portfolio Page</h1>;
+  }
+  
---
+++ src/app/pages/solutions.jsx
@@ -0,0 +1,4 @@
+export default function Solutions() {
+    return <h1>Solutions Page</h1>;
+  }
+  
